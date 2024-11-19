beea7a09688294064f2e6991c3f5d65bef201eac
LiuvaSalas <liuvasalas01@gmail.com>
2024-11-18 22:36:34 -0300

    Se agregaron los modelso region y comuna para agregarlos al formulario posteriormente.

--- Inmobiliaria/gestion_inmuebles/models.py
+++ Inmobiliaria/gestion_inmuebles/models.py
@@ -2,6 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Region(models.Model):
+    identificador = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Comuna(models.Model):
+    nombre = models.CharField(max_length=50)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
 class Inmueble(models.Model):
     nombre = models.CharField(max_length=200)
     descripcion = models.TextField()
@@ -11,7 +27,7 @@ class Inmueble(models.Model):
     habitaciones = models.IntegerField()
     banos = models.IntegerField()
     direccion = models.CharField(max_length=200)
-    comuna = models.CharField(max_length=100)
+    comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
     tipo_inmueble = models.CharField(max_length=50)
     precio_mensual = models.DecimalField(max_digits=10, decimal_places=2)
     arrendador = models.ForeignKey(User, on_delete=models.CASCADE)
