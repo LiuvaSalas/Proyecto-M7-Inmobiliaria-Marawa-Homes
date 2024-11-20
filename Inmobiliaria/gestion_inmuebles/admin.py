047fc58d7389f83a746dc70ebc76a0d082d1d86a
LiuvaSalas <liuvasalas01@gmail.com>
2024-11-19 22:53:36 -0300

    Actualizacion de representacion en el panel de administracion. Screenshots Hito 2

--- Inmobiliaria/gestion_inmuebles/admin.py
+++ Inmobiliaria/gestion_inmuebles/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
-from .models import Inmueble, Region, Comuna
+from .models import Inmueble, Region, Comuna, Clasificacion_Inmueble
+from django.utils import timezone
 
-admin.site.register(Inmueble)
+
+@admin.register(Inmueble)
+class InmuebleAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "direccion", "precio_mensual")
+    search_fields = ("nombre", "direccion", "tipo_inmueble__tipo_inmueble")
+    list_filter = ("precio_mensual", "region__nombre")
+    readonly_fields = ("fecha_creacion", "ultima_modificacion")
+
+    def save_model(self, request, obj, form, change):
+        if change:
+            obj.ultima_modificacion = timezone.now()
+        else:
+            obj.fecha_creacion = timezone.now()
+            obj.save()
+
+
 admin.site.register(Region)
-admin.site.register(Comuna)
+
+
+@admin.register(Comuna)
+class ComunaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "get_region")
+    search_fields = ("region__nombre", "nombre")
+
+
+admin.site.register(Clasificacion_Inmueble)
--- Inmobiliaria/gestion_inmuebles/models.py
+++ Inmobiliaria/gestion_inmuebles/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
 from usuarios.models import Usuario
 
 
@@ -18,6 +17,16 @@ class Comuna(models.Model):
     def __str__(self):
         return self.nombre
 
+    def get_region(self):
+        return self.region.nombre
+
+
+class Clasificacion_Inmueble(models.Model):
+    tipo_de_inmueble = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tipo_de_inmueble
+
 
 class Inmueble(models.Model):
     nombre = models.CharField(max_length=200)
@@ -28,11 +37,14 @@ class Inmueble(models.Model):
     habitaciones = models.IntegerField()
     banos = models.IntegerField()
     direccion = models.CharField(max_length=200)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
     comuna = models.ForeignKey(Comuna, on_delete=models.CASCADE)
-    tipo_inmueble = models.CharField(max_length=50)
+    tipo_inmueble = models.ForeignKey(Clasificacion_Inmueble, on_delete=models.CASCADE)
     precio_mensual = models.DecimalField(max_digits=10, decimal_places=2)
     arrendador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     arrendada = models.BooleanField(blank=True, null=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    ultima_modificacion = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nombre
