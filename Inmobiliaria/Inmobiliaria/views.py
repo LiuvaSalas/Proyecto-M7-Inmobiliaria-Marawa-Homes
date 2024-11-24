1b625b769d26c1667d97f3bb30724e69e99b31f2
LiuvaSalas <liuvasalas01@gmail.com>
2024-11-23 21:30:58 -0300

    -Se modificaron vistas en index

--- Inmobiliaria/Inmobiliaria/views.py
+++ Inmobiliaria/Inmobiliaria/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import redirect, render
 from gestion_inmuebles.models import Inmueble
 from django.db.models import Q
+from django.db.models import Count
 
 
 def index(request):
     inmuebles_disponibles = Inmueble.objects.filter(arrendada=False)
     inmuebles_destacados = Inmueble.objects.filter(arrendada=False).order_by('-m2_construidos')[:3]
-    contexto = {"inmuebles_disponibles": inmuebles_disponibles, "inmuebles_destacados":inmuebles_destacados}
+    cuenta_inmuebles_la_serena = (Inmueble.objects.filter(arrendada=False, comuna = 29).values('comuna__nombre').annotate(total = Count('id')).order_by('-total'))
+    cuenta_inmuebles_vina = (Inmueble.objects.filter(arrendada=False, comuna = 41).values('comuna__nombre').annotate(total = Count('id')).order_by('-total'))
+    cuenta_inmuebles_arica = (Inmueble.objects.filter(arrendada=False, comuna = 2).values('comuna__nombre').annotate(total = Count('id')).order_by('-total'))
+    cuenta_inmuebles_santiago = (Inmueble.objects.filter(arrendada=False, comuna = 51).values('comuna__nombre').annotate(total = Count('id')).order_by('-total'))
+    cuenta_inmuebles_concepcion = (Inmueble.objects.filter(arrendada=False, comuna = 77).values('comuna__nombre').annotate(total = Count('id')).order_by('-total'))
+    print(cuenta_inmuebles_santiago)
+    contexto = {"inmuebles_disponibles": inmuebles_disponibles, "inmuebles_destacados":inmuebles_destacados, "cuenta_inmuebles_la_serena":cuenta_inmuebles_la_serena,"cuenta_inmuebles_vina":cuenta_inmuebles_vina,"cuenta_inmuebles_arica":cuenta_inmuebles_arica,"cuenta_inmuebles_santiago":cuenta_inmuebles_santiago,"cuenta_inmuebles_concepcion":cuenta_inmuebles_concepcion}
     return render(request, "index.html", contexto)
 
 
